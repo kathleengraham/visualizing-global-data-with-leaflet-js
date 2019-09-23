c100ccb7b863d6cf8269be4625123c3167cb0b8c
Kathleen Graham <kathleeng77@outlook.com>
2019-09-22 19:00:53 -0500

    added wine map layer

---
+++ wine.js
@@ -0,0 +1,136 @@
+// link to maps with api in config.js
+const mapboxLink = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}';
+
+// create satellite map layer
+const satmap = L.tileLayer(mapboxLink,{
+    attribution: attribution,
+    maxZoom: 18,
+    id: 'mapbox.satellite',
+    accessToken: API_KEY
+    });
+
+// create light map layer
+const lightmap = L.tileLayer(mapboxLink,{
+    attribution: attribution,
+    maxZoom: 18,
+    id: 'mapbox.light',
+    accessToken: API_KEY
+});
+
+// create dark map layer
+const darkmap = L.tileLayer(mapboxLink,{
+    attribution: attribution,
+    maxZoom: 18,
+    id: 'mapbox.dark',
+    accessToken: API_KEY
+});
+
+// create basemap layer with the other maps
+const baseMaps = {
+    'Satellite Map': satmap,
+    'Light Map': lightmap,
+    'Dark Map': darkmap
+};
+
+// make variable for wineLayer to adjust later
+var wineLayer;
+// set countryColor based on consumption of wine
+// color choices from http://colorbrewer2.org/?type=sequential&scheme=Purples&n=5
+function countryColor(d) {
+    return d > 1000000 ? '#54278f' :
+        d > 100000 ? '#756bb1' :
+        d > 10000 ? '#9e9ac8' :
+        d > 1000 ? '#cbc9e2' :
+        d > 100 ? '#f2f0f7' :
+        'white';
+}
+
+// fxn for filling in the countries
+function style(feature) {
+    return {
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7,
+        fillColor: countryColor(feature.properties.wineConsumption)
+    };
+}
+
+// fxn for highlighting outline of country on hover
+function highlightFeature(e) {
+    let layer = e.target;
+
+    layer.setStyle({
+        weight: 5,
+        color: '#666',
+        dashArray: '',
+        fillOpacity: 0.7
+    });
+
+    if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
+        layer.bringToFront();
+    }
+
+    info.update(layer.feature.properties);
+}
+
+// fxn to reset the outline of countries when not hovering anymore
+function resetHighlight(e) {
+    wineLayer.resetStyle(e.target);
+    info.update();
+}
+
+// fxn to zoom in to each country once clicked
+function zoomToFeature(e) {
+    myMap.fitBounds(e.target.getBounds());
+}
+
+// fxn to bring all previous feature fxns together
+function onEachFeature(feature, layer) {
+    layer.on({
+        mouseover: highlightFeature,
+        mouseout: resetHighlight,
+        click: zoomToFeature
+    });
+}
+
+// create wine layer that includes outlines, highlight, resethighlight, and click to zoom
+wineLayer = L.geoJson(wineData, {
+    style: style,
+    onEachFeature: onEachFeature
+});
+
+// create overlays
+const mapOverlay = {
+    'Wine': wineLayer
+};
+
+// load satmap and outline as default
+const myMap = L.map('map', {
+    center: [0,0],
+    zoom: 3,
+    layers: [lightmap, wineLayer]
+});
+
+// add all map layers
+L.control.layers(baseMaps, mapOverlay, {
+    collapsed: true
+}).addTo(myMap);
+
+// control that shows country info on hover
+let info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info');
+    this.update();
+    return this._div;
+};
+
+info.update = function (props) {
+    this._div.innerHTML = '<h4>World Wine Consumption</h4>' +  (props ?
+        '<b>' + props.name + '</b><br />' + props.wineConsumption + ' L'
+        : 'Hover over a country');
+};
+
+info.addTo(myMap);
